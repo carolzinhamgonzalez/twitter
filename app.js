e943dc02e0215198e3301b05226db776d7e4fdd1
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-07-09 14:07:50 -0300

    Versão 0.0.2

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 var btnTweet = document.getElementById('btn');
+var txtArea = document.getElementById('msg');
+
+function check() {
+  checkEmpty();
+  caracteres();
+}
+
+function caracteres() {
+  var number = document.querySelector('.counter');
+  var counter = number.innerHTML = 140 - txtArea.value.length;
+}
+
+function checkEmpty(){
+  var btn = document.getElementById('btn');
+  if (txtArea.value ==''){
+    btn.disabled = true;
+  } else if(txtArea.value !=''){
+    btn.disabled = false;
+  }
+}
+
 
 function tweetar(){
-  var text = document.getElementById('msg').value;
+  var text = txtArea.value;
   var sectionTweets = document.querySelector('.ListTweets');
   var tweet = document.createElement('article');
       // var html = '<article class="tweet"> %msg%</article>';
       // var newHtml = html.replace('%msg%', text);
       // document.querySelector('.ListTweets').insertAdjacentHTML('beforeend', newHtml);
-  tweet.classList.add("tweet");
-  var paragraph = document.createElement("p");
+  tweet.classList.add('tweet');
+  var paragraph = document.createElement('p');
   paragraph.innerHTML = text;
   tweet.appendChild(paragraph);
   sectionTweets.appendChild(tweet);
   document.getElementById('msg').value = '';
 }
 
-btnTweet.addEventListener("click", tweetar);
+txtArea.addEventListener('keyup', check);
+btnTweet.addEventListener('click', tweetar);
