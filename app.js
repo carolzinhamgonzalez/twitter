0af4e7c6d650cf0f6d606ba3ccdc2ca89c647239
Carol Gonzalez <carolzinha.mgonzalez@gmail.com>
2018-07-09 11:40:01 -0300

    0.0.1 form e tweetar

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-// var form = document.createElement("form");
-// var textarea = document.createElement("textarea");
-// form.appendChild(textarea);
-// // var id = document.createElement("id");
-// // var placeholder = document.createElement("placeholder");
-// textarea.setAttribute("id", "msg");
-// textarea.setAttribute("placeholder", "O que está acontecendo?");
-// var button = document.createElement("button");
-// form.appendChild(button);
-// // var type = document.createElement("type");
-// // var name = document.createElement("name");
-// button.setAttribute("type", "submit");
-// button.setAttribute("name", "tweetar");
-// // button.setAttribute(value, "");
-// button.innerHTML = "Tweetar"
-var button = document.querySelectorAll("button");
-button.addEventListener("click", tweetar)
+var btnTweet = document.getElementById('btn');
 
 function tweetar(){
   var text = document.getElementById('msg').value;
-  var div = document.createElement("div");
-  body.appendChild(div);
-  div.classList.add("tweet");
+  var sectionTweets = document.querySelector('.ListTweets');
+  var tweet = document.createElement('article');
+      // var html = '<article class="tweet"> %msg%</article>';
+      // var newHtml = html.replace('%msg%', text);
+      // document.querySelector('.ListTweets').insertAdjacentHTML('beforeend', newHtml);
+  tweet.classList.add("tweet");
   var paragraph = document.createElement("p");
-  div.appendChild(paragraph);
   paragraph.innerHTML = text;
+  tweet.appendChild(paragraph);
+  sectionTweets.appendChild(tweet);
+  document.getElementById('msg').value = '';
 }
+
+btnTweet.addEventListener("click", tweetar);
